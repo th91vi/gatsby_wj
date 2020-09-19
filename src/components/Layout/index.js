6ab2bb34a1bdb2c14364e9038627149a00a2c7d0
Thiago Vieira <th91v@outlook.com>
2020-09-19 12:18:46 -0300

    feat: added styled-components; added new Layout component and styles

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import * as S from './styles'
+import GlobalStyles from '../../styles/global'
+
+const Layout = ({ children }) => {
+  return (
+    <S.LayoutWrapper>
+      <GlobalStyles />
+      <S.LayoutMain>
+        {children}
+      </S.LayoutMain>
+    </S.LayoutWrapper>
+
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
