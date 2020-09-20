eb0f414edf942bbafe376ec4e4c6879331ab3b16
Thiago Vieira <th91v@outlook.com>
2020-09-20 13:51:44 -0300

    feat:: added SocialLinks component

--- src/components/Sidebar/Index.js
+++ src/components/Sidebar/Index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from '../Profile'
+import SocialLinks from '../SocialLinks'
 
 import * as S from './styles'
 
@@ -7,6 +8,7 @@ const Sidebar = () => {
   return (
     <S.SidebarWrapper>
       <Profile />
+      <SocialLinks />
     </S.SidebarWrapper>
   )
 }
